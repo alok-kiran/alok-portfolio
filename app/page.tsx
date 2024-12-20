6a219cba4d5db9cf425fd47d666c0f71e18ade46
alok-kiran <alokkiran777@gmail.com>
2024-12-20 19:40:49 +0400

    added project section

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Header from "./sections/header";
 import Hero from "./sections/heroes";
+import Projects from "./sections/projects";
 
 export default function Home() {
   return (
     <div >
       <Header />
       <Hero />
+      <Projects />
     </div>
   );
 }
